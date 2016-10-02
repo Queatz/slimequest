b9f99d496ed7f106b71db227cdb5a99f49a659c7
Jacob Ferrero <jacobaferrero@gmail.com>
2016-10-02 14:39:27 -0700

    Update blocking tiles list

--- shared/src/main/java/com/slimequest/shared/MapTiles.java
+++ shared/src/main/java/com/slimequest/shared/MapTiles.java
@@ -15,27 +15,36 @@ public class MapTiles {
     static {
         // TODO defined by tileset specification...
         // Needs support for groups
+        // Format = y 00 x
         collideTiles.add(3000);
-        collideTiles.add(4000);
-        collideTiles.add(5000);
-        collideTiles.add(6000);
-        collideTiles.add(7000);
-
-        collideTiles.add(4001);
-        collideTiles.add(5001);
-        collideTiles.add(6001);
-        collideTiles.add(7001);
 
         collideTiles.add(3002);
-        collideTiles.add(4002);
-        collideTiles.add(5002);
-        collideTiles.add(6002);
-        collideTiles.add(7002);
 
-        collideTiles.add(4003);
-        collideTiles.add(5003);
+        collideTiles.add(4);
+        collideTiles.add(1004);
+        collideTiles.add(6004);
+        collideTiles.add(7004);
 
-        collideTiles.add(4004);
-        collideTiles.add(5004);
+        collideTiles.add(5);
+        collideTiles.add(1005);
+        collideTiles.add(2005);
+        collideTiles.add(3005);
+        collideTiles.add(5005);
+        collideTiles.add(6005);
+        collideTiles.add(7005);
+
+        collideTiles.add(6);
+        collideTiles.add(1006);
+        collideTiles.add(3006);
+        collideTiles.add(5006);
+        collideTiles.add(6006);
+        collideTiles.add(7006);
+
+        collideTiles.add(7);
+        collideTiles.add(1007);
+        collideTiles.add(2007);
+        collideTiles.add(3007);
+        collideTiles.add(5007);
+        collideTiles.add(6007);
     }
 }
